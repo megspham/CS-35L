3f7bd63d1b5761f6f1741d9b7b8517c7364d60ec
Megan Pham <61036308+megspham@users.noreply.github.com>
2020-12-13 04:46:29 -0800

    Add files via upload

---
+++ assignment 7/dlsubmission/randmain.mk
@@ -0,0 +1,15 @@
+
+
+
+randmain: randcpuid.c randmain.c randcpuid.h 
+	$(CC) $(CFLAGS) -c randcpuid.c -o randcpuid.o
+	$(CC) $(CFLAGS) -c randmain.c -o randmain.o
+	$(CC) $(CFLAGS) randmain.o randcpuid.o -o randmain -ldl -Wl,-rpath=$(PWD)
+
+randlibhw.so: randlibhw.c randlib.h
+	$(CC) $(CFLAGS) -fPIC -c randlibhw.c -o randlibhw.o
+	$(CC) $(CFLAGS) -shared randlibhw.o -o randlibhw.so
+
+randlibsw.so: randlibsw.c randlib.h
+	$(CC) $(CFLAGS) -fPIC -c randlibsw.c -o randlibsw.o
+	$(CC) $(CFLAGS) -shared randlibsw.o -o randlibsw.so
